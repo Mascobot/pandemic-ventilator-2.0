5c8f38d46fd2c033478e058d0d728b5d69122b24
Mascobot <m@mascobot.com>
2020-04-04 17:04:55 -0700

    Update Covid_ventilator.py

--- code/desktop/Covid_ventilator.py
+++ code/desktop/Covid_ventilator.py
@@ -27,7 +27,7 @@ def get_port():
     ports = serial.tools.list_ports.comports()
     for i in ports:
         i = str(i)
-        if 'Arduino' in i:     
+        if 'AMA' in i:     
             return (i.split()[0])
             
 def open_serial():
